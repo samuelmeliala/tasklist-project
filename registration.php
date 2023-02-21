4a47fe3901fe478e00000a64c3ade3d19be32118
Samuel Benediktus Meliala <brunodargo.bella@gmail.com>
2023-02-21 17:02:25 +0700

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <h1>Register Here</h1>
+    <form action="" method="post">
+        <table">
+            <tr>
+                <td><label for="name">Name </label></td>
+                <td><input type="text" name="name" id="name"></td>
+            </tr>
+            <tr>
+                <td><label for="username">Username </label></td>
+                <td><input type="text" name="username" id="username"></td>
+            </tr>
+            <tr>
+                <td><label for="password">Password </label></td>
+                <td><input type="password" name="password" id="password"></td>
+            </tr>
+            <tr>
+                <td><label for="password2">Confirm Password </label></td>
+                <td><input type="password" name="password2" id="password2"></td>
+            </tr>
+            <tr>
+                <td><button type="submit" name="register">Register Now</button></td>
+            </tr>
+        </table>
+    </form>
+</body>
+
+</html>
